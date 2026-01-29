c53cad2d0ea36dff8401d68cc88c81a2110c9497
João Victor Vial <joaovictorvial@gmail.com>
2026-01-29 08:09:33 -0300

    Versao final do dataset e ETL

--- data/etl.py
+++ data/etl.py
@@ -1,36 +1,62 @@
 import pandas as pd
+import os
 import re
 
-INPUT_PATH = "data/raw/2025_12.csv"
-
+RAW_DIR = "data/raw"
 OUTPUT_PATH = "data/processed/ipca_grupos_regioes_long.csv"
 
-df = pd.read_csv(INPUT_PATH, sep=";", encoding="utf-8")
+dfs = []
 
-df.rename(columns={df.columns[0]: "grupo"}, inplace=True)
+for filename in os.listdir(RAW_DIR):
+    if not filename.endswith(".csv"):
+        continue
 
-df["grupo"] = df["grupo"].str.replace(r"^\d+\.", "", regex=True)
+    match = re.match(r"(\d{4})_(\d{2})\.csv", filename)
+    if not match:
+        continue
 
-df["grupo"] = df["grupo"].str.strip()
+    ano = int(match.group(1))
+    mes = int(match.group(2))
 
-df_long = df.melt(
-    id_vars="grupo",
-    var_name="regiao",
-    value_name="variacao"
+    file_path = os.path.join(RAW_DIR, filename)
+
+    df = pd.read_csv(file_path, sep=";", encoding="utf-8")
+
+    df.rename(columns={df.columns[0]: "grupo"}, inplace=True)
+
+    df["grupo"] = df["grupo"].str.replace(r"^\d+\.", "", regex=True)
+    df["grupo"] = df["grupo"].str.strip()
+
+    df_long = df.melt(
+        id_vars="grupo",
+        var_name="regiao",
+        value_name="variacao"
+    )
+
+    df_long = df_long.dropna(subset=["variacao"])
+
+    df_long["variacao"] = (
+        df_long["variacao"]
+        .astype(str)
+        .str.replace(",", ".")
+        .astype(float)
+    )
+
+    df_long["regiao"] = df_long["regiao"].str.strip()
+
+    df_long["ano"] = ano
+    df_long["mes"] = mes
+
+    df_long = df_long[["ano", "mes", "grupo", "regiao", "variacao"]]
+
+    dfs.append(df_long)
+
+df_final = pd.concat(dfs, ignore_index=True)
+
+df_final.to_csv(
+    OUTPUT_PATH,
+    index=False,
+    sep="\t"
 )
-
-df_long = df_long.dropna(subset=["variacao"])
-
-df_long["variacao"] = df_long["variacao"].astype(str)
-df_long["variacao"] = (
-    df_long["variacao"]
-    .str.replace(",", ".")
-    .astype(float)
-)
-
-df_long["regiao"] = df_long["regiao"].str.strip()
-
-df_long.to_csv(OUTPUT_PATH, index=False)
-
-print("ETL finalizado com sucesso!")
-print(f"Arquivo gerado em: {OUTPUT_PATH}")
+print("ETL FINALIZADO COM SUCESSO")
+print(f"Linhas geradas: {df_final.shape[0]}")
