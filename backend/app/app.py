158b1824f25648df9a43827938e67e079937925b
João Victor Vial <joaovictorvial@gmail.com>
2026-02-02 09:29:42 -0300

    Setup docker pronto

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,11 +1,23 @@
-from flask import Flask, Response
-from data_loader import load_data
+from flask import Flask, jsonify
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
+BASE_DIR = os.getcwd()  
+CSV_PATH = os.path.join(
+    BASE_DIR,
+    "data",
+    "processed",
+    "ipca_grupos_regioes_long.csv"
+)
+
 @app.route("/api/ipca")
 def ipca():
-    return load_data()
+    print("Lendo CSV em:", CSV_PATH)
+
+    df = pd.read_csv(CSV_PATH, sep="\t")
+    return jsonify(df.to_dict(orient="records"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
